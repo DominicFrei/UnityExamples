b5fcf0cf98bb8bc94c61648b91d83db66575cb21
Dominic Frei <business.dominic.frei@gmail.com>
2020-10-12 18:33:14 +0100

    Add an Audio example.

---
+++ Assets/Audio/Audio.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio : MonoBehaviour
+{
+    [SerializeField] private AudioClip _audioClip = null;
+
+    void Start()
+    {
+        // Play the clip at a point.
+        AudioSource.PlayClipAtPoint(_audioClip, new Vector3(1, 1, 1));
+        StartCoroutine(PlayAudio());
+    }
+
+    private IEnumerator PlayAudio()
+    {
+        // Alternative: Play the AudioSource's attached AudioClip.
+        while(true)
+        {
+            yield return new WaitForSeconds(2.0f);
+            GetComponent<AudioSource>().Play();
+        }
+    }
+}
---
+++ Assets/Audio/_README.cs
@@ -0,0 +1,16 @@
+﻿/**
+ * Exmaple: Audio
+ * 
+ * Playing audio can be done in multiple ways. We use audio clips to play audio.
+ * Those AudioClips can be played at a specific location (i.e. a vector) or played from an attached AudioSource.
+ * 
+ * The example consists of one GameObject:
+ * - just an empty GameObject
+ * 
+ * Setting up the GameObject:
+ * - add a script component: Audio.cs
+ * - use the audio clip in the folder as the GameObjects's AudioClip
+ * - add an AudioSource component, using the audio clip in the folder as its AudioClip
+ * Everything else is done in code.
+ * 
+ */
